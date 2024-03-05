8f6c1e476edd0267e4ed946fa2ec281b3f361124
Starking7 <aniyinyinsamuel03@gmail.com>
2024-03-05 01:21:11 +0100

    Snacks Website Project

---
+++ about.js
@@ -0,0 +1,8 @@
+// var openav = document.getElementById("opennav")
+var links = document.getElementById("links")
+
+function opennav() {
+    // console.log("Hello World");
+    links.classList.toggle("show")
+}
+
---
+++ main.js
@@ -0,0 +1,7 @@
+// var openav = document.getElementById("opennav")
+var links = document.getElementById("links")
+
+function opennav() {
+    // console.log("Hello World");
+    links.classList.toggle("show")
+}
